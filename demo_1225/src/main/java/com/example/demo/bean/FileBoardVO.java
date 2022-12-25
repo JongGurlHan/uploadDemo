111b2bf562edbb7b649a98a662e0ddaefa91e968
jghan <gojkhan13@gmail.com>
2022-12-26 00:34:17 +0900

    게시글 등록

---
+++ demo_1225/src/main/java/com/example/demo/bean/FileBoardVO.java
@@ -0,0 +1,51 @@
+package com.example.demo.bean;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class FileBoardVO {
+  
+  private int b_no;
+  private String title;
+  private String content;
+  private String writer;
+  private Date reg_date;
+  
+	public int getB_no() {
+		return b_no;
+	}
+	public void setB_no(int b_no) {
+		this.b_no = b_no;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public Date getReg_date() {
+		return reg_date;
+	}
+	public void setReg_date(Date reg_date) {
+		this.reg_date = reg_date;
+	}
+
+}
